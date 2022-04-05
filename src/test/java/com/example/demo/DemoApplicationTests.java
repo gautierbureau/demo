c9da4edf60523e9d7838795b3a8303a0a6022dd8
Gautier Bureau <gautier.bureau@rte-france.com>
2022-04-05 16:23:52 +0200

    Fix CI with testcontainers.

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,9 +2,29 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+
+    @Container
+    public static PostgreSQLContainer<?> postgreDBContainer = new PostgreSQLContainer<>("postgres:13.1")
+            .withDatabaseName("demo");
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.liquibase.url", postgreDBContainer::getJdbcUrl);
+        registry.add("spring.liquibase.user", postgreDBContainer::getUsername);
+        registry.add("spring.liquibase.password", postgreDBContainer::getPassword);
+        registry.add("spring.datasource.url", postgreDBContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreDBContainer::getUsername);
+        registry.add("spring.datasource.password", postgreDBContainer::getPassword);
+    }
 
     @Test
     void contextLoads() {
