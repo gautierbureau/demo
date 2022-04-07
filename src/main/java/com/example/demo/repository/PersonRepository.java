e721889ed4a1b98f7f3b9e29d74a7bb4e831335c
Gautier Bureau <gautier.bureau@rte-france.com>
2022-04-07 10:15:27 +0200

    Fix repository.

--- src/main/java/com/example/demo/repository/PersonRepository.java
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -3,8 +3,8 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.PersonEntity;
+import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
-public interface PersonRepository extends JpaRepository<PersonEntity, UUID> {
+@Repository
+public interface PersonRepository extends JpaRepository<PersonEntity, Integer> {
 }
